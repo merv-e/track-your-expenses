8a973ec399ce8e2313bace5873225f9506c05a4f
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-12 15:19:37 +0200

    Components are again splitted into smaller chunks and more styling is added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -23,35 +24,10 @@ function App() {
     },
   ];
 
-  // console.log(expenses[1].date);
-
   return (
     <div>
-      <h1>Track Your Expanses!</h1>
-      <ExpenseItem 
-       id={expenses[0].id}
-       title={expenses[0].title} 
-       amount={expenses[0].amount}
-       date={expenses[0].date}
-      />
-      <ExpenseItem 
-       id={expenses[1].id}
-       title={expenses[1].title} 
-       amount={expenses[1].amount}
-       date={expenses[1].date}
-      />
-      <ExpenseItem 
-       id={expenses[2].id}
-       title={expenses[2].title} 
-       amount={expenses[2].amount}
-       date={expenses[2].date}
-      />
-      <ExpenseItem 
-       id={expenses[3].id}
-       title={expenses[3].title} 
-       amount={expenses[3].amount}
-       date={expenses[3].date}
-      />
+      <h1>Track Your Expenses</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
