e426202210d9938b929ed8b2a2c055501606d306
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-12 15:01:44 +0200

    - some changes in the UI are made - ExpenseItem component is splitted into two components by considering seperation of concerns - ExpenseDate component is created as a child component of ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
+const ExpenseItem = props => {
   return (
     <div className="expense-item"> 
-        <div>Date</div>
-        <div className="expense-item__description">
-            <h2>Title</h2>
-            <div className="expense-item__price">Amount</div>
-        </div>
-
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+      </div>
     </div>
   )
 }
 
 export default ExpenseItem
+
