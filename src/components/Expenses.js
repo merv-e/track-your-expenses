6e531d7be49ffe40527d54f4ddb9dad499425ba5
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-12 16:28:47 +0200

    - A wrapper component is created (Card) and  some components are wrapped by this component - with this wrapper component, a css file is also created and since some css content was shared among components instead of using these styles more than once, it's only going to be used with the wrapper component's css file.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,35 +1,22 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem';
 import "./Expenses.css"
+import Card from './Card';
+
 
 const Expenses = (props) => {
   return (
-    <div className='expenses'>
-      <ExpenseItem
-        id={props.expenses[0].id}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        id={props.expenses[1].id}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        id={props.expenses[2].id}
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        id={props.expenses[3].id}
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </div>
+    <Card className='expenses'>
+        { props.expenses.map( e => 
+            <ExpenseItem
+              key={e.id}
+              id={e.id}
+              title={e.title}
+              amount={e.amount}
+              date={e.date}
+            />
+        )}
+    </Card>    
   );
 }
 
