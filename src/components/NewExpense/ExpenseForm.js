a327c566e4f62cde74fd1a24e0a8b1aa23f5b67a
merv-e <34655454+merv-e@users.noreply.github.com>
2023-05-15 16:30:43 +0200

    ...

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,45 +5,58 @@ const ExpenseForm = () => {
 
   // You can use two ways of using useState:
 
+  const [titleInput, setTitleInput] = useState("");
+  const [amountInput, setAmountInput] = useState("");
+  const [dateInput, setDateInput] = useState("");
+  
   // one way is using three useState statements for title, amount and date values and update them accordingly. ex : const [title, setTitle] = useState(""); etc...
 
   // and the other way is, as shown below, using a single state to hold all user inputs and updating the values this way.
 
-  const [inputValue, setInputValue] = useState({
-    titleInput : "",
-    amountInput : "",
-    dateInput : "",
-  });
+  // const [inputValue, setInputValue] = useState({
+  //   titleInput : "",
+  //   amountInput : "",
+  //   dateInput : "",
+  // });
 
-  const handleTitleChange = (ev) => {
-    setInputValue((prevState) => {
-      return { 
-        ...prevState, 
-        titleInput: ev.target.value 
-      };
-    });
-  };
+  // const handleTitleChange = (ev) => {
+  //   setInputValue((prevState) => {
+  //     return { 
+  //       ...prevState, 
+  //       titleInput: ev.target.value 
+  //     };
+  //   });
+  // };
 
-  const handleAmountChange = (ev) => {
-   setInputValue((prevState) => {
-      return {
-        ...prevState, 
-        amountInput: ev.target.value,
-      }
-    });
-  };
+  // const handleAmountChange = (ev) => {
+  //  setInputValue((prevState) => {
+  //     return {
+  //       ...prevState, 
+  //       amountInput: ev.target.value,
+  //     }
+  //   });
+  // };
 
-  const handleDateChange = (ev) => {
-    setInputValue((prevState) => {
-      return {
-        ...prevState, 
-        dateInput: ev.target.value
-      }
-    });
-  };
+  // const handleDateChange = (ev) => {
+  //   setInputValue((prevState) => {
+  //     return {
+  //       ...prevState, 
+  //       dateInput: ev.target.value
+  //     }
+  //   });
+  // };
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
+
+    const expenseData = {
+      title: titleInput,
+      amount: amountInput,
+      date: new Date(dateInput),
+    };
+
+    console.log(expenseData);
+
     // setTitle("");
     // setDate("");
     // setAmount("");
@@ -56,8 +69,8 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input
             type="text"
-            value={inputValue.titleInput}
-            onChange={handleTitleChange}
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
           />
         </div>
 
@@ -67,8 +80,8 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            value={inputValue.amountInput}
-            onChange={handleAmountChange}
+            value={amountInput}
+            onChange={(e) => setAmountInput(e.target.value)}
           />
         </div>
 
@@ -78,15 +91,15 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2022-12-31"
-            value={inputValue.dateInput}
-            onChange={handleDateChange}
+            value={dateInput}
+            onChange={(e) => setDateInput(e.target.value)}
           />
         </div>
       </div>
 
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
     </form>
   );
 }
